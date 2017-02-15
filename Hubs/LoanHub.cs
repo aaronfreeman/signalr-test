76177ef986b603323f628739eb655bb9d007d835
Aaron Freeman <aaron@freeman.zone>
2017-02-15 14:10:06 -0600

    adding lookup function

--- Hubs/LoanHub.cs
+++ Hubs/LoanHub.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace signalr_test.Hubs
 {
@@ -15,6 +17,17 @@ namespace signalr_test.Hubs
             {
                 Clients.Group(loanNumber).broadcastMessage(name);
             }
+        }
+
+        private Dictionary<long,Person> people = new Dictionary<long,Person> {
+            {1, new Person {Id = 1, Name = "John"}},
+            {2, new Person {Id = 2, Name = "Tracy"}},
+            {3, new Person {Id = 3, Name = "BJ"}}
+        };
+
+        public Person LookupPerson(long id) 
+        {
+            return people.ContainsKey(id) ? people[id] : new Person();
         }
     }
 }
---
+++ Hubs/Person.cs
@@ -0,0 +1,9 @@
+
+namespace signalr_test.Hubs
+{
+    public class Person
+    {
+      public string Name {get;set;}
+      public long Id {get;set;}
+    }
+}
